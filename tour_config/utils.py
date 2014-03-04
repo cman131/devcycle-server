524769b65e68b92b4dfc91f508b3343a2825e264
root <root@cycl-ops.se.rit.edu>
2014-03-04 16:03:56 -0500

    Fixed spacing

--- tour_config/utils.py
+++ tour_config/utils.py
@@ -3,8 +3,7 @@ from tour_config.models import TourConfig
 
 
 def set_polling_rate():
-	""" Set the polling rate in cache """
-
+    """ Cache the Polling Rate """
     #Set Polling Rate in Cache on Creation
     tour = TourConfig.objects.latest('pk') # Get the latest tour
     cache.set(settings.JSON_KEYS['POLLING_RATE'], tour.polling_rate)
