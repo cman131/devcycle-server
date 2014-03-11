5bb59676f5bedff78157ed35ecc80e06df28c30d
Chris Ketant <ketantchris@gmail.com>
2014-03-11 18:13:48 -0400

    Updating load tests

--- load_test/load_test.rb
+++ load_test/load_test.rb
@@ -14,7 +14,7 @@ example_params = JSON.parse(IO.read('example.json'))
 Benchmark.bm(25) do |x|
   x.report do
     for i in 1..32000
-      req = Typhoeus::Request.new("http://devcycle.se.rit.edu/location_update", params: example_params)
+      req = Typhoeus::Request.new("http://devcycle.se.rit.edu/location_update/", params: example_params)
 
       req.on_complete do |response|
         done = done+1
@@ -27,4 +27,3 @@ Benchmark.bm(25) do |x|
     hydra.run
   end
 end
-
