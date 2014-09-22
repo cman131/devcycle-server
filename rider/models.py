a76a6b1b84ffcc79a48e8892490a8198d85e0d89
jeklund <eklund.joshua@gmail.com>
2014-09-21 20:23:10 -0400

    Attempting to add a relationship class in the Rider model along with the Affinity Group

--- rider/models.py
+++ rider/models.py
@@ -16,3 +16,11 @@ class Rider(models.Model):
 class RiderAffinityGroupRelationship(models.Model):
 	rider = models.ForeignKey(Rider)
 	affinitygroup = models.ForeignKey(AffinityGroup)
+
+class Affinity_Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    #Regex is to ensure A-Z or a-z or digits. It also ensures a minumum length of at least 2 '{2,}'
+    #Django does not have a min_length field.
+    code = models.CharField(unique=True, max_length=4, validators=[RegexValidator(regex='^[a-zA-Z\d]{2,}$', message='Affinity Group Code can only be digits & letters and min length of 2')])
+    description = models.CharField(max_length=255, null=True, blank=True) 
+    registered_at = models.DateTimeField(auto_now_add=True,editable=False)
