1e960c8595cbd66c4c09aabb51ab4284a314171d
jeklund <eklund.joshua@gmail.com>
2014-09-25 22:03:37 -0400

    Added Group name as it did not yet exist.

--- affinity/models.py
+++ affinity/models.py
@@ -7,6 +7,7 @@ from django.core.validators import RegexValidator
 
 class Group(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
     #Regex is to ensure A-Z or a-z or digits. It also ensures a minumum length of at least 3 '{3,}'
     #Django does not have a min_length field.
     code = models.CharField(unique=True, max_length=7, validators=[RegexValidator(regex='^[a-zA-Z\d]{3,}$', message='Affinity Group Code can only be digits & letters and min length of 3')])
