3328248f0f4d56f5826051d54039d0bca90a931f
TOM MOORE <tmoore@moyatech.com>
2014-10-22 17:39:01 -0400

    * Updated list_group_view to return a JsonResponse instead of an HttpResponse * Added definitions for joining and leaving groups

--- rider/views.py
+++ rider/views.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from rider.models import Rider, Affinity_Group_Mapping
 from affinity.models import Group
 from rider.serializer import riderSerializer
@@ -8,25 +9,29 @@ from rest_framework import status
 from rider.rider_id_tools import create_uuid, decrypt_uuid
 from django.conf import settings
 from django.core.cache import cache
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 logger = logging.getLogger(__name__)
 
 def list_group_view(request, r_id):
-	response = HttpResponse()
+	json_data = []
 	aff_data = Affinity_Group_Mapping.objects.filter(rider=r_id)
 	for r in aff_data:
 	    aff_id = r['affinity_group']
 		group_data = Group.objects.get(id=aff_id)
-		response.write("<name>"+group_data.name+"</name>")
-		response.write("<id>"+aff_id+"</id>")
-	return response
+		json_data.add({'name':group_data.name,'id':aff_id})
+	return JsonResponse(json.dumps(json_data))
 def create_group_view(request, name, aff_id, r_id):
     
 def join_group_view(request, aff_id, r_id):
-
+	agm = Affinity_Group_Mapping(rider=r_id,affinity_group=aff_id)
+	agm.save()
+	return HttpResponse(status=status.HTTP_201_CREATED)
 def leave_group_view(request, aff_id, r_id):
-
+	agm = Affinity_Group_Mapping.get(rider=r_id).filter(affinity_group=aff_id)
+	agm.delete()
+	return HttpResponse(status=status.HTTP_201_CREATED)
+	
 class RiderAPI(APIView):
 
     def post(self, request, format=None):
